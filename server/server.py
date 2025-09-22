988d2a6d74666c3c44dffca4beac9525394c4832
mathewgeola <mathewgeola@gmail.com>
2025-09-22 15:11:40 +0800

    v1

---
+++ server/server.py
@@ -0,0 +1,44 @@
+import os
+import ssl
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+server_cert = os.path.join(os.getcwd(), "certs", "server-cert.cer")
+server_key = os.path.join(os.getcwd(), "certs", "server-key.key")
+client_cert = os.path.join(os.getcwd(), "certs", "client-cert.cer")
+
+print("server_cert:", server_cert)
+print("server_key:", server_key)
+print("client_cert:", client_cert)
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def _writeheaders(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+    def do_GET(self):
+        self._writeheaders()
+        self.wfile.write("OK".encode("utf-8"))
+
+
+def main():
+    port = int(sys.argv[1]) if len(sys.argv) == 2 else 443
+    server_address = ("0.0.0.0", port)
+
+    server = HTTPServer(server_address, RequestHandler)  # type: ignore
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_verify_locations(client_cert)
+
+    server.socket = context.wrap_socket(server.socket, server_side=True, do_handshake_on_connect=False)
+
+    print(f"Starting server, listen at: {server_address[0]}:{server_address[1]}")
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
